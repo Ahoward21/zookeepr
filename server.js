34b70e85fa8f6a779804549c4cb692487e41a049
GrumpyMonk <anhoward1988@gmail.com>
2022-01-02 21:51:14 -0800

    requests for specific animals by ID added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// https://enigmatic-waters-17154.herokuapp.com/api/animals
 const express = require('express');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
@@ -43,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+  // request for specific animal
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
 
 app.listen(PORT, () => {
